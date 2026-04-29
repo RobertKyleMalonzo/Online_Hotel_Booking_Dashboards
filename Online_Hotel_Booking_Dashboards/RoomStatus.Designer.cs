00ad49757425ebb9a065e9ca99266aaeccb867f7
Cute <Cute@HOVER-LAPTOP-3>
2026-04-30 04:38:22 +0800

    roomstatus#1

---
+++ Online_Hotel_Booking_Dashboards/RoomStatus.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Online_Hotel_Booking_Dashboards
+{
+    partial class RoomStatus
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RoomStatus));
+            panel1 = new Panel();
+            btnReports = new Button();
+            btnPayments = new Button();
+            pictureBox6 = new PictureBox();
+            button5 = new Button();
+            btnStaff = new Button();
+            btnUsers = new Button();
+            btnReservation = new Button();
+            btnRooms = new Button();
+            label7 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.MidnightBlue;
+            panel1.Controls.Add(btnReports);
+            panel1.Controls.Add(btnPayments);
+            panel1.Controls.Add(pictureBox6);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(btnStaff);
+            panel1.Controls.Add(btnUsers);
+            panel1.Controls.Add(btnReservation);
+            panel1.Controls.Add(btnRooms);
+            panel1.Controls.Add(label7);
+            panel1.Location = new Point(4, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(219, 621);
+            panel1.TabIndex = 4;
+            // 
+            // btnReports
+            // 
+            btnReports.FlatAppearance.BorderSize = 0;
+            btnReports.FlatStyle = FlatStyle.Flat;
+            btnReports.ForeColor = Color.White;
+            btnReports.Location = new Point(16, 450);
+            btnReports.Name = "btnReports";
+            btnReports.Size = new Size(200, 46);
+            btnReports.TabIndex = 18;
+            btnReports.Text = "📊  Reports";
+            btnReports.TextAlign = ContentAlignment.MiddleLeft;
+            btnReports.UseVisualStyleBackColor = true;
+            // 
+            // btnPayments
+            // 
+            btnPayments.FlatAppearance.BorderSize = 0;
+            btnPayments.FlatStyle = FlatStyle.Flat;
+            btnPayments.ForeColor = Color.White;
+            btnPayments.Location = new Point(16, 398);
+            btnPayments.Name = "btnPayments";
+            btnPayments.Size = new Size(200, 46);
+            btnPayments.TabIndex = 17;
+            btnPayments.Text = "💳  Payments";
+            btnPayments.TextAlign = ContentAlignment.MiddleLeft;
+            btnPayments.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(33, 21);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(137, 128);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 16;
+            pictureBox6.TabStop = false;
+            // 
+            // button5
+            // 
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.ForeColor = Color.White;
+            button5.Location = new Point(16, 502);
+            button5.Name = "button5";
+            button5.Size = new Size(200, 46);
+            button5.TabIndex = 15;
+            button5.Text = "➜]  Logout";
+            button5.TextAlign = ContentAlignment.MiddleLeft;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // btnStaff
+            // 
+            btnStaff.FlatAppearance.BorderSize = 0;
+            btnStaff.FlatStyle = FlatStyle.Flat;
+            btnStaff.ForeColor = Color.White;
+            btnStaff.Location = new Point(16, 346);
+            btnStaff.Name = "btnStaff";
+            btnStaff.Size = new Size(200, 46);
+            btnStaff.TabIndex = 14;
+            btnStaff.Text = "👤  Staff";
+            btnStaff.TextAlign = ContentAlignment.MiddleLeft;
+            btnStaff.UseVisualStyleBackColor = true;
+            // 
+            // btnUsers
+            // 
+            btnUsers.FlatAppearance.BorderSize = 0;
+            btnUsers.FlatStyle = FlatStyle.Flat;
+            btnUsers.ForeColor = Color.White;
+            btnUsers.Location = new Point(16, 294);
+            btnUsers.Name = "btnUsers";
+            btnUsers.Size = new Size(200, 46);
+            btnUsers.TabIndex = 13;
+            btnUsers.Text = "👤  Users";
+            btnUsers.TextAlign = ContentAlignment.MiddleLeft;
+            btnUsers.UseVisualStyleBackColor = true;
+            // 
+            // btnReservation
+            // 
+            btnReservation.FlatAppearance.BorderSize = 0;
+            btnReservation.FlatStyle = FlatStyle.Flat;
+            btnReservation.ForeColor = Color.White;
+            btnReservation.Location = new Point(16, 242);
+            btnReservation.Name = "btnReservation";
+            btnReservation.Size = new Size(200, 46);
+            btnReservation.TabIndex = 12;
+            btnReservation.Text = "📅  Reservations";
+            btnReservation.TextAlign = ContentAlignment.MiddleLeft;
+            btnReservation.UseVisualStyleBackColor = true;
+            // 
+            // btnRooms
+            // 
+            btnRooms.FlatAppearance.BorderSize = 0;
+            btnRooms.FlatStyle = FlatStyle.Flat;
+            btnRooms.ForeColor = Color.White;
+            btnRooms.Location = new Point(16, 190);
+            btnRooms.Name = "btnRooms";
+            btnRooms.Size = new Size(200, 46);
+            btnRooms.TabIndex = 11;
+            btnRooms.Text = "🛏  Rooms";
+            btnRooms.TextAlign = ContentAlignment.MiddleLeft;
+            btnRooms.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.White;
+            label7.Location = new Point(47, 164);
+            label7.Name = "label7";
+            label7.Size = new Size(103, 23);
+            label7.TabIndex = 10;
+            label7.Text = "My Account";
+            // 
+            // RoomStatus
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1125, 606);
+            Controls.Add(panel1);
+            Name = "RoomStatus";
+            Text = "RoomStatus";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button btnReports;
+        private Button btnPayments;
+        private PictureBox pictureBox6;
+        private Button button5;
+        private Button btnStaff;
+        private Button btnUsers;
+        private Button btnReservation;
+        private Button btnRooms;
+        private Label label7;
+    }
+}
